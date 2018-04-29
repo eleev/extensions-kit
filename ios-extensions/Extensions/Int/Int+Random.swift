e638da56a77099e93a46bdf9d657eb9f131c4070
Astemir Eleev <astemireleev@gmail.com>
2018-04-29 19:49:10 +0300

    Implemented Int+Random extension

---
+++ ios-extensions/Extensions/Int/Int+Random.swift
@@ -0,0 +1,27 @@
+//
+//  Int+Random.swift
+//  ios-extensions
+//
+//  Created by Astemir Eleev on 29/04/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+public extension Int {
+    
+    // MARK: - Methods
+    
+    static func random(in range: ClosedRange<Int>) -> Int {
+        var offset = 0
+        
+        if range.lowerBound < 0 {
+            offset = abs(range.lowerBound)
+        }
+        
+        let mini = UInt32(range.lowerBound + offset)
+        let maxi = UInt32(range.upperBound + offset)
+        
+        return Int(mini + arc4random_uniform(maxi - mini)) - offset
+    }
+}
