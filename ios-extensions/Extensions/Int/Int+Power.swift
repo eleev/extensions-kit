e4d0b230a636f7c65b525348d4c5a0cabd7e9cf9
Astemir Eleev <astemireleev@gmail.com>
2018-04-29 19:48:56 +0300

    Implemented Int+Power operator extension

---
+++ ios-extensions/Extensions/Int/Int+Power.swift
@@ -0,0 +1,18 @@
+//
+//  Int+Power.swift
+//  ios-extensions
+//
+//  Created by Astemir Eleev on 29/04/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+precedencegroup PowerPrecedence { higherThan: MultiplicationPrecedence }
+infix operator ^^ : PowerPrecedence
+
+func ^^(radix: Int, power: Int) -> Int {
+    return Int(pow(Double(radix), Double(power)))
+}
+
+
