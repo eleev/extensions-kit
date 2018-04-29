905b05758537eb31154cd1556660b24aee3cd81d
Astemir Eleev <astemireleev@gmail.com>
2018-04-29 19:35:33 +0300

    Initial Commit

---
+++ ios-extensions/ios_extensions.h
@@ -0,0 +1,19 @@
+//
+//  ios_extensions.h
+//  ios-extensions
+//
+//  Created by Astemir Eleev on 29/04/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ios_extensions.
+FOUNDATION_EXPORT double ios_extensionsVersionNumber;
+
+//! Project version string for ios_extensions.
+FOUNDATION_EXPORT const unsigned char ios_extensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ios_extensions/PublicHeader.h>
+
+
