389dae515fdaec3c5de280e9ecc89ec5d3c323c3
Astemir Eleev <astemireleev@gmail.com>
2018-04-29 19:48:29 +0300

    Implemented ClosedRange+Random extension

---
+++ ios-extensions/Extensions/ClosedRange/ClosedRange+Random.swift
@@ -0,0 +1,30 @@
+//
+//  Range+Random.swift
+//  ios-extensions
+//
+//  Created by Astemir Eleev on 29/04/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+public extension ClosedRange {
+    
+    // MARK: - Properties
+    
+    public var randomInt: Int {
+        get {
+            var offset = 0
+            
+            if (lowerBound as! Int) < 0 {
+                offset = abs(lowerBound as! Int)
+            }
+            
+            let mini = UInt32(lowerBound as! Int + offset)
+            let maxi = UInt32(upperBound as! Int + offset)
+            
+            return Int(mini + arc4random_uniform(maxi - mini)) - offset
+        }
+    }
+    
+}
