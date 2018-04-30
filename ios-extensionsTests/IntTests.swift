f6bbf142dcd8a9fec09b7754e169a09d39b615b5
Astemir Eleev <astemireleev@gmail.com>
2018-04-30 10:23:11 +0300

    Covered Int type with tests

--- ios-extensionsTests/IntTests.swift
+++ ios-extensionsTests/IntTests.swift
@@ -22,9 +22,6 @@ class IntTests: XCTestCase {
     }
     
     func testDigits() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        
         let ten = 15
         let tenResult = ten.digitCount
         assert(tenResult == 2)
@@ -52,6 +49,58 @@ class IntTests: XCTestCase {
         
         let twentySeven = 3^^3
         assert(twentySeven == 27)
+    }
+    
+    func testFactorial() {
+        var factorial = 1.factorial()
+        assert(factorial == 1)
+
+        factorial = 2.factorial()
+        assert(factorial == 2)
+
+        factorial = 3.factorial()
+        assert(factorial == 6)
+        
+        factorial = 4.factorial()
+        assert(factorial == 24)
+        
+        factorial = 5.factorial()
+        assert(factorial == 120)
+        
+        factorial = 6.factorial()
+        assert(factorial == 720)
+        
+        factorial = 7.factorial()
+        assert(factorial == 5040)
+        
+        factorial = 8.factorial()
+        assert(factorial == 40320)
+    }
+    
+    func testEvenOdd() {
+        var isOdd = 2.isOdd()
+        assert(!isOdd)
+        
+        isOdd = 4.isOdd()
+        assert(!isOdd)
+        
+        var isEven = 7.isEven()
+        assert(!isEven)
+        
+        isEven = 6347859.isEven()
+        assert(!isEven)
+    }
+    
+    func testClamp() {
+        // Upper bound
+        let ten = 10
+        let tenRes = ten.clamp(min: 0, 5)
+        assert(tenRes == 5)
+        
+        // Lower bound
+        let five = 5
+        let fiveRes = five.clamp(min: 7, 12)
+        assert(fiveRes == 7)
     }
     
     func testPerformanceExample() {
