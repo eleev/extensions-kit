860c43384732811ff1384965e0d539faffb3badf
Astemir Eleev <astemireleev@gmail.com>
2018-04-30 10:22:38 +0300

    Implemented UIImage+SolidColor extension

---
+++ ios-extensions/Extensions/UIImage/UIImage+SolidColor.swift
@@ -0,0 +1,32 @@
+//
+//  UIImage+SolidColor.swift
+//  ios-extensions
+//
+//  Created by Astemir Eleev on 30/04/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - Extension for creating UIImage from a UIColor
+public extension UIImage {
+    
+    public convenience init?(color: UIColor, size: CGSize = CGSize(width: 1, height: 1)) {
+        let rect = CGRect(origin: .zero, size: size)
+        UIGraphicsBeginImageContextWithOptions(rect.size, false, 0.0)
+        color.setFill()
+        UIRectFill(rect)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        guard let cgImage = image?.cgImage else { return nil }
+        self.init(cgImage: cgImage)
+    }
+    
+    func isImageWhite() -> Bool? {
+        guard let whiteImage = UIImage(color: .white, size: self.size), let whiteImageData = UIImageJPEGRepresentation(whiteImage, 1.0), let imageData = UIImageJPEGRepresentation(self, 1.0) else {
+            return nil
+        }
+        return whiteImageData == imageData
+    }
+}
