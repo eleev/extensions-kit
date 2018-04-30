9de69ccb9eab143598ff1eec04682062d60abf4a
Astemir Eleev <astemireleev@gmail.com>
2018-04-30 10:22:24 +0300

    Implemented UIImage+Resize extension

---
+++ ios-extensions/Extensions/UIImage/UIImage+Resize.swift
@@ -0,0 +1,49 @@
+//
+//  UIImage+Resize.swift
+//  ios-extensions
+//
+//  Created by Astemir Eleev on 30/04/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - Class-level extension for UIImage that allow to resize input image based on expected image width or/and height
+public extension UIImage {
+    
+    class func resize(_ image: UIImage, newWidth: CGFloat) -> UIImage? {
+        
+        let scale = newWidth / image.size.width
+        let newHeight = image.size.height * scale
+        UIGraphicsBeginImageContext(CGSize(width: newWidth, height: newHeight))
+        image.draw(in: CGRect(x: 0, y: 0, width: newWidth, height: newHeight))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage
+    }
+    
+    class func resize(_ image: UIImage, newHeight: CGFloat) -> UIImage? {
+        
+        let scale = newHeight / image.size.height
+        let newWidth = image.size.width * scale
+        UIGraphicsBeginImageContext(CGSize(width: newWidth, height: newHeight))
+        image.draw(in: CGRect(x: 0, y: 0, width: newWidth, height: newHeight))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage
+    }
+    
+    class func resize(_ image: UIImage, newSize: CGSize) -> UIImage? {
+        
+        UIGraphicsBeginImageContext(newSize)
+        image.draw(in: CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage
+    }
+    
+}
+
