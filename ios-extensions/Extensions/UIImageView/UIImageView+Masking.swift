b2438385043e7caff49bab6fa135b04b2efce573
Astemir Eleev <astemireleev@gmail.com>
2018-04-30 10:22:54 +0300

    Implemented UIImageView+Masking extension

---
+++ ios-extensions/Extensions/UIImageView/UIImageView+Masking.swift
@@ -0,0 +1,20 @@
+//
+//  UIImageView+Masking.swift
+//  ios-extensions
+//
+//  Created by Astemir Eleev on 30/04/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import UIKit
+
+public extension UIImageView {
+    
+    func mask(with image: UIImage, targetImageSize: CGSize) {
+        let maskLayer = CALayer()
+        maskLayer.contents = image.cgImage
+        maskLayer.frame = CGRect(x: 0, y: 0, width: targetImageSize.width, height: targetImageSize.height)
+        self.layer.mask = maskLayer
+        self.layer.masksToBounds = true
+    }
+}
