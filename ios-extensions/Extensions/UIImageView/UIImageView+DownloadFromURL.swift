d21c11f07e630e80c9b2ff72b2be19131748e77a
Astemir Eleev <astemireleev@gmail.com>
2018-05-01 09:18:53 +0300

    Implemented UIImageView+DownloadFromURL extension

---
+++ ios-extensions/Extensions/UIImageView/UIImageView+DownloadFromURL.swift
@@ -0,0 +1,32 @@
+//
+//  UIImageView+DownloadFromURL.swift
+//  ios-extensions
+//
+//  Created by Astemir Eleev on 01/05/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import UIKit
+
+extension UIImageView {
+    
+    func downloadImageFrom(url: String, with defaultImage: UIImage? = nil, completion: @escaping(_ image: UIImage)->Void) {
+        guard let url = URL(string: url) else { return }
+        URLSession.shared.dataTask(with: url, completionHandler: { data, response, error -> Void in
+            
+            guard let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+                else {
+                    if let defaultImage = defaultImage {
+                        completion(defaultImage)
+                    }
+                    return
+            }
+            
+            completion(image)
+        }).resume()
+    }
+    
+}
