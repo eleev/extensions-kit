8725ee4ef812d64014baefd2d32f31713b5e886f
Astemir Eleev <astemireleev@gmail.com>
2018-05-03 09:57:48 +0300

    Added Int+Roman extension

---
+++ ios-extensions/Extensions/Foundation/Int/Int+Roman.swift
@@ -0,0 +1,35 @@
+//
+//  Int+Roman.swift
+//  ios-extensions
+//
+//  Created by Astemir Eleev on 03/05/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+extension Int {
+    
+    public func romanNumeral() -> String? {
+        guard self > 0 else {
+            return nil
+        }
+        let romanValues = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+        let arabicValues = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        
+        var romanValue = ""
+        var startingValue = self
+        
+        for (index, romanChar) in romanValues.enumerated() {
+            let arabicValue = arabicValues[index]
+            let div = startingValue / arabicValue
+            if div > 0 {
+                for _ in 0..<div {
+                    romanValue += romanChar
+                }
+                startingValue -= arabicValue * div
+            }
+        }
+        return romanValue
+    }
+}
