e5303836e3f4cd34bb75a138cd00ee964e7c8fe5
Astemir Eleev <astemireleev@gmail.com>
2018-05-03 10:38:43 +0300

    Implemented Dictionary+JSON extension

---
+++ ios-extensions/Extensions/Foundation/Dictionary/Dictionary+JSON.swift
@@ -0,0 +1,27 @@
+//
+//  Dictionary+JSON.swift
+//  ios-extensions
+//
+//  Created by Astemir Eleev on 03/05/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+public extension Dictionary {
+    
+    public func jsonData(prettify: Bool = false) -> Data? {
+        guard JSONSerialization.isValidJSONObject(self) else {
+            return nil
+        }
+        let options = (prettify == true) ? JSONSerialization.WritingOptions.prettyPrinted : JSONSerialization.WritingOptions()
+        return try? JSONSerialization.data(withJSONObject: self, options: options)
+    }
+    
+    public func jsonString(prettify: Bool = false) -> String? {
+        guard JSONSerialization.isValidJSONObject(self) else { return nil }
+        let options = (prettify == true) ? JSONSerialization.WritingOptions.prettyPrinted : JSONSerialization.WritingOptions()
+        guard let jsonData = try? JSONSerialization.data(withJSONObject: self, options: options) else { return nil }
+        return String(data: jsonData, encoding: .utf8)
+    }
+}
