1bf3cefeb21f3c98736ff3a8ec73b30ab0474fba
Astemir Eleev <astemireleev@gmail.com>
2018-05-11 17:07:55 +0300

    Implemented MutableCollection+Shuffle collection

---
+++ ios-extensions/Extensions/Foundation/MutableCollection/MutableCollection+Shuffle.swift
@@ -0,0 +1,23 @@
+//
+//  MutableCollection+Shuffle.swift
+//  ios-extensions
+//
+//  Created by Astemir Eleev on 11/05/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+public extension MutableCollection {
+    
+    /// In-place shuffling of self
+    public mutating func shuffle() {
+        if count < 2 { return }
+        
+        for i in indices.dropLast() {
+            let dist = distance(from: i, to: endIndex)
+            let j = index(i, offsetBy: numericCast(arc4random_uniform(numericCast(dist))))
+            swapAt(i, j)
+        }
+    }
+}
