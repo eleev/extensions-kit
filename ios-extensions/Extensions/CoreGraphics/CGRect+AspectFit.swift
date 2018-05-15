367963811478c2d523b1b42434b821aaed06fd1e
Astemir Eleev <astemireleev@gmail.com>
2018-05-15 13:53:16 +0300

    Implemented CGRect+AspectFit

---
+++ ios-extensions/Extensions/CoreGraphics/CGRect+AspectFit.swift
@@ -0,0 +1,29 @@
+//
+//  CGRect+AspectFit.swift
+//  ios-extensions
+//
+//  Created by Astemir Eleev on 15/05/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import CoreGraphics
+
+extension CGRect {
+    
+    func aspectFit(inRect target: CGRect) -> CGRect {
+        let scale: CGFloat = {
+            let scale = target.width / self.width
+
+            return self.height * scale <= target.height ?
+                scale :
+                target.height / self.height
+        }()
+
+        let width = self.width * scale
+        let height = self.height * scale
+        let x = target.midX - width / 2
+        let y = target.midY - height / 2
+        
+        return CGRect(x: x, y: y, width: width, height: height)
+    }
+}
