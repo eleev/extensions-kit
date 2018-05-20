053d9bf6cff13c0065fc00866cc965e95a193415
Astemir Eleev <astemireleev@gmail.com>
2018-05-20 12:58:41 +0300

    Implemented SKEmitterNode+AdvanceSimulation extension

---
+++ ios-extensions/Extensions/SpriteKit/SKEmitterNode/SKEmitterNode+AdvanceSimulation.swift
@@ -0,0 +1,29 @@
+//
+//  SKEmitterNode+AdvanceSimulation.swift
+//  ios-extensions
+//
+//  Created by Astemir Eleev on 20/05/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import SpriteKit.SKEmitterNode
+
+extension SKEmitterNode {
+    
+    /// Safely advance the simulation by checking if the node was previously paused and then advancing the simulation
+    ///
+    /// - Parameter sec: advance simulation in seconds
+    func safeAdvanceSimulationTime(_ sec: TimeInterval) {
+        let emitterPaused = self.isPaused
+        
+        if emitterPaused {
+            self.isPaused = false
+        }
+        advanceSimulationTime(sec)
+        
+        if emitterPaused {
+            self.isPaused = true
+        }
+    }
+}
+
