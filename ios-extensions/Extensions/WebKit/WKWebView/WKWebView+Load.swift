20822dae7d65c6d0834971980042d0d0759ec6b4
Astemir Eleev <astemireleev@gmail.com>
2018-05-25 08:51:21 +0300

    Implemented WKWebView+Load extension

---
+++ ios-extensions/Extensions/WebKit/WKWebView/WKWebView+Load.swift
@@ -0,0 +1,19 @@
+//
+//  WKWebView+Load.swift
+//  ios-extensions
+//
+//  Created by Astemir Eleev on 25/05/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import WebKit
+
+extension WKWebView {
+    
+    func load(_ urlString: String) {
+        if let url = URL(string: urlString) {
+            let request = URLRequest(url: url)
+            load(request)
+        }
+    }
+}
