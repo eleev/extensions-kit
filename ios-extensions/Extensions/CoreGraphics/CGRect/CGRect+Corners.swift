6f282e1fdae19ae210a19468413fc4e0e9d137a7
Astemir Eleev <astemireleev@gmail.com>
2018-06-05 16:15:48 +0300

    Implemented CGRect+Corners extension

---
+++ ios-extensions/Extensions/CoreGraphics/CGRect/CGRect+Corners.swift
@@ -0,0 +1,28 @@
+//
+//  CGRect+Corners.swift
+//  ios-extensions
+//
+//  Created by Astemir Eleev on 05/06/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import CoreGraphics
+
+public extension CGRect {
+    
+    public var topLeft: CGPoint{
+        return origin
+    }
+    
+    public var topRight: CGPoint{
+        return CGPoint(x: origin.x + width, y: origin.y)
+    }
+    
+    public var bottomLeft: CGPoint{
+        return CGPoint(x: origin.x, y: origin.y + height)
+    }
+    
+    public var bottomRight: CGPoint{
+        return CGPoint(x: origin.x + width, y: origin.y + height)
+    }
+}
