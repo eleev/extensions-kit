b05afd751e309fc1d3098c444bc0ffb96c22f53a
Astemir Eleev <astemireleev@gmail.com>
2018-06-05 16:20:03 +0300

    Implemented UIImage+ImageFromUIView extension

---
+++ ios-extensions/Extensions/UIKit/UIImage/UIImage+ImageFromUIView.swift
@@ -0,0 +1,23 @@
+//
+//  UIImage+ImageFromUIView.swift
+//  ios-extensions
+//
+//  Created by Astemir Eleev on 05/06/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import UIKit.UIImage
+import UIKit.UIView
+
+public extension UIImage {
+    
+    public class func image(from view: UIView) -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(view.bounds.size, false, 0.0)
+        guard let context = UIGraphicsGetCurrentContext() else { return nil }
+        
+        view.layer.render(in: context)
+        let img = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return img
+    }
+}
