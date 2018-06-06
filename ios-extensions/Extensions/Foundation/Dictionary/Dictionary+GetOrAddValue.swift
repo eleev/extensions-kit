fd51f9e20057eae27b2ca03031f846e631945539
Astemir Eleev <astemireleev@gmail.com>
2018-06-06 10:06:25 +0300

    Implemented Dictionary+GetOrAddValue extension

---
+++ ios-extensions/Extensions/Foundation/Dictionary/Dictionary+GetOrAddValue.swift
@@ -0,0 +1,23 @@
+//
+//  Dictionary+GetOrAddValue.swift
+//  ios-extensions
+//
+//  Created by Astemir Eleev on 06/06/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+public extension Dictionary {
+    
+    /// Checks for a value for a given key or creates a new key/value pair if none was found
+    public mutating func value(for key: Key, orAdd valueClosure: @autoclosure () -> Value) -> Value {
+        if let value = self[key] {
+            return value
+        }
+        
+        let value = valueClosure()
+        self[key] = value
+        return value
+    }
+}
