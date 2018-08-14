a9746e986927f1bf2fe5dca89a8922ea5bc565bd
Astemir Eleev <astemireleev@gmail.com>
2018-08-14 16:32:08 +0300

    Refactored random(min:, max:) -> Int implementation

--- ios-extensions/Extensions/Foundation/Int/Int+Random.swift
+++ ios-extensions/Extensions/Foundation/Int/Int+Random.swift
@@ -26,7 +26,7 @@ public extension Int {
     }
     
     static func random(min: Int = 0, max: Int) -> Int {
-        return Int(arc4random_uniform(UInt32((max - min) + 1))) + min
+        return Int(arc4random_uniform(UInt32(max - min))) + min
     }
     
 }
