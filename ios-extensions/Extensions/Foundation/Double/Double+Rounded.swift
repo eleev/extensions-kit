4558e47f48d2f1df6220edadc385d81807aab384
Astemir Eleev <astemireleev@gmail.com>
2018-08-27 12:34:54 +0300

    Refactored Double+Rounded extension: changed access modifier from `internal`  to `public`

--- ios-extensions/Extensions/Foundation/Double/Double+Rounded.swift
+++ ios-extensions/Extensions/Foundation/Double/Double+Rounded.swift
@@ -8,9 +8,9 @@
 
 import CoreGraphics
 
-extension Double {
+public extension Double {
     /// Rounds the double to decimal places value
-    func rounded(toPlaces places:Int) -> Double {
+    public func rounded(toPlaces places:Int) -> Double {
         let divisor = pow(10.0, Double(places))
         return (self * divisor).rounded() / divisor
     }
