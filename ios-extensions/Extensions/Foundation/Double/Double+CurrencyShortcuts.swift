dfce04dc4f69f76cd1da1a53495862b985c0670e
Astemir Eleev <astemireleev@gmail.com>
2018-08-27 12:34:25 +0300

    Implemented Double+CurrencyShortcuts extension

---
+++ ios-extensions/Extensions/Foundation/Double/Double+CurrencyShortcuts.swift
@@ -0,0 +1,31 @@
+//
+//  Double+CurrencyShortcuts.swift
+//  ios-extensions
+//
+//  Created by Astemir Eleev on 27/08/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+public extension Double {
+    
+    public var usd: String? {
+        return formatter(for: "en_US_POSIX")
+    }
+    
+    public var ruble: String? {
+        return formatter(for: "ru_RU")
+    }
+    
+    // MARK: - Private methods
+    
+    private func formatter(for identifier: String) -> String? {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = Locale(identifier: identifier)
+        
+        let number = NSNumber(value: self)
+        return formatter.string(from: number)
+    }
+}
