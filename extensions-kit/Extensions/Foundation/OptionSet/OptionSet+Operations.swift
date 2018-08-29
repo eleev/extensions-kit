00ecc00baf0d11327dcfee256ad7bdb593d0900b
Astemir Eleev <astemireleev@gmail.com>
2018-08-29 10:33:54 +0300

    Implemented OptionSet extensions

---
+++ extensions-kit/Extensions/Foundation/OptionSet/OptionSet+Operations.swift
@@ -0,0 +1,27 @@
+//
+//  OptionSet+Operations.swift
+//  extensions-kit
+//
+//  Created by Astemir Eleev on 29/08/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - Extension that adds missing operations
+public extension OptionSet where Element == Self {
+    
+    /// Duplicates the set, inserts the new element and returns Self
+    public func inserting(new element: Self) -> Self {
+        var opts = self
+        opts.insert(element)
+        return opts
+    }
+    
+    /// Duplicates the set, removes the element and returns Self
+    public func removing(_ element: Self) -> Self {
+        var opts = self
+        opts.remove(element)
+        return opts
+    }
+}
