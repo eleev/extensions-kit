a15f83b8f7381edc2f9e87ef31efcd5d9cb8f198
Astemir Eleev <astemireleev@gmail.com>
2018-09-07 16:56:48 +0300

    Implemented Array+Union extension

---
+++ extensions-kit/Extensions/Foundation/Array/Array+Union.swift
@@ -0,0 +1,28 @@
+//
+//  Array+Union.swift
+//  extensions-kit
+//
+//  Created by Astemir Eleev on 07/09/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+public extension Array where Element: Equatable {
+    
+    /// Unions self and the input arrays
+    ///
+    /// - Parameter values: is a vararg for Arrays
+    /// - Returns: a new Array containing merged elements
+    public func union(values: [Element]...) -> Array {
+        var result = self
+        
+        for array in values {
+            for value in array where !result.contains(value) {
+                result.append(value)
+            }
+        }
+        
+        return result
+    }
+}
