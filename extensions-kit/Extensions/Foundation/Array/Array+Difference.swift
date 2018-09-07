c3b79a9c9359607f6c277e6c048bfd09e1710f06
Astemir Eleev <astemireleev@gmail.com>
2018-09-07 16:57:57 +0300

    Implemented Array+Difference extension

---
+++ extensions-kit/Extensions/Foundation/Array/Array+Difference.swift
@@ -0,0 +1,30 @@
+//
+//  Array+Difference.swift
+//  extensions-kit
+//
+//  Created by Astemir Eleev on 07/09/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+public extension Array where Element: Equatable {
+    
+    /// Computes differences between self and the input arrays
+    ///
+    /// - Parameter elements: is a vararg of arrays
+    /// - Returns: array that contains difference
+    public func difference(elements: [Element]...) -> [Element] {
+        var result = [Element]()
+        
+        link: for item in self {
+            for element in elements where element.contains(item) {
+                continue link
+            }
+            result.append(item)
+        }
+        
+        return result
+    }
+    
+}
