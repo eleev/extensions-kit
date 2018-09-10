213e50fe47055b36158898ebf0a4c79378f4725c
Astemir Eleev <astemireleev@gmail.com>
2018-09-10 08:52:04 +0300

    Created NSLayoutConstraint+Activation extension

---
+++ extensions-kit/Extensions/UIKit/NSLayoutConstraint+Activation/NSLayoutConstraint+Activation.swift
@@ -0,0 +1,24 @@
+//
+//  NSLayoutConstraint+Activation.swift
+//  extensions-kit
+//
+//  Created by Astemir Eleev on 10/09/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import UIKit
+
+public extension NSLayoutConstraint {
+    
+    /// Sets the priority of the constraint to the specified value and then return self
+    @discardableResult public func with(priority: UILayoutPriority) -> Self {
+        self.priority = priority
+        return self
+    }
+    
+    /// Set activation state for the current layout constrint and return self
+    @discardableResult public func set(active: Bool) -> Self {
+        isActive = active
+        return self
+    }
+}
