5c670be55a682d85eb4becc2dac0f3087185cb56
Astemir Eleev <astemireleev@gmail.com>
2018-09-17 17:48:45 +0300

    Implemented SpinLock class

---
+++ extensions-kit/Extensions/Grand Central Dispatch/Locks/SpinLock.swift
@@ -0,0 +1,27 @@
+//
+//  SpinLock.swift
+//  extensions-kit
+//
+//  Created by Astemir Eleev on 17/09/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+/// A lock which causes a thread trying to acquire it to simply wait in a loop ("spin") while repeatedly checking if the lock is available
+final public class SpinLock: LockType {
+    
+    // MARK: - Properties
+    
+    private var unfairLock = os_unfair_lock()
+    
+    // MARK: - Conformance to LockType protocol
+    
+    public func lock() {
+        os_unfair_lock_lock(&unfairLock)
+    }
+    
+    public func unlock() {
+        os_unfair_lock_unlock(&unfairLock)
+    }
+}
