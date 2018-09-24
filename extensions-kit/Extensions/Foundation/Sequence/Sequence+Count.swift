7cedfb3376962d4d936ae1d25eda2dc361c17e36
Astemir Eleev <astemireleev@gmail.com>
2018-09-24 10:12:53 +0300

    Implemented Sequence+Count extension

---
+++ extensions-kit/Extensions/Foundation/Sequence/Sequence+Count.swift
@@ -0,0 +1,22 @@
+//
+//  Sequence+Count.swift
+//  extensions-kit
+//
+//  Created by Astemir Eleev on 24/09/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+public extension Sequence {
+    
+    /// Performs a logical condition and counts all true results
+    ///
+    /// - Parameter logicalExpression: is a closure that performs contains a logical condition
+    /// - Returns: the number of true logical expressions
+    public func count(_ logicalExpression: (_ element: Element) -> Bool) -> Int {
+        var counter = 0
+        for element in self where logicalExpression(element) { counter += 1 }
+        return counter
+    }
+}
