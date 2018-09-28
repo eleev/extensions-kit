78196daeab53606df1e71f827a8def7e87cf62db
Astemir Eleev <astemireleev@gmail.com>
2018-09-28 14:23:36 +0300

    Implemented Date+Date+FirstLast and Date+PreviousNext extensions

---
+++ extensions-kit/Extensions/Foundation/Date/Date+FirstLast.swift
@@ -0,0 +1,38 @@
+//
+//  Date+FirstLast.swift
+//  extensions-kit
+//
+//  Created by Astemir Eleev on 28/09/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+public extension Date {
+    
+    public var firstDayOfWeek: Date {
+        var beginningOfWeek = Date()
+        var interval = TimeInterval()
+        
+        _ = Calendar.current.dateInterval(of: .weekOfYear, start: &beginningOfWeek, interval: &interval, for: self)
+        return beginningOfWeek
+    }
+    
+    public var startOfDay: Date {
+        return Calendar.current.startOfDay(for: self)
+    }
+    
+    public var endOfDay: Date {
+        let cal = Calendar.current
+        var components = DateComponents()
+        components.day = 1
+        return cal.date(byAdding: components, to: self.startOfDay)!.addingTimeInterval(-1)
+    }
+    
+    public var numberOfDaysInMonth: Int {
+        let calendar = Calendar.current
+        let dayRange = (calendar as NSCalendar).range(of: NSCalendar.Unit.day, in: NSCalendar.Unit.month, for: self)
+        
+        return dayRange.length
+    }
+}
---
+++ extensions-kit/Extensions/Foundation/Date/Date+PreviousNext.swift
@@ -0,0 +1,22 @@
+//
+//  Date+PreviousNext.swift
+//  extensions-kit
+//
+//  Created by Astemir Eleev on 28/09/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+public extension Date {
+    
+    public func nextDate() -> Date {
+        let nextDate = Calendar.current.date(byAdding: .day, value: 1, to: self)
+        return nextDate ?? Date()
+    }
+    
+    public func previousDate() -> Date {
+        let previousDate = Calendar.current.date(byAdding: .day, value: -1, to: self)
+        return previousDate ?? Date()
+    }
+}
