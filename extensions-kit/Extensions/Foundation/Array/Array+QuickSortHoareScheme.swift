07ad738451b81b438d2d7d62a9184bc48c4951b8
Astemir Eleev <astemireleev@gmail.com>
2018-09-29 10:44:27 +0300

    Implemented Array+QuickSortHoareScheme and Array+QuickSortLomutoScheme extensions

---
+++ extensions-kit/Extensions/Foundation/Array/Array+QuickSortHoareScheme.swift
@@ -0,0 +1,43 @@
+//
+//  Array+QuickSortHoareScheme.swift
+//  extensions-kit
+//
+//  Created by Astemir Eleev on 29/09/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+public extension Array where Element: Comparable {
+    
+    public static func quickSortHoare(array: inout [Element], lowest: Int, highest: Int) {
+        
+        if lowest < highest {
+            let pivot = Array.partitionHoare(array: &array, lowest: lowest, highest: highest)
+            
+            Array.quickSortHoare(array: &array, lowest: lowest, highest: pivot)
+            Array.quickSortHoare(array: &array, lowest: pivot + 1, highest: highest)
+        } else {
+            debugPrint(#function + " lowest param is bigger than highest: ", lowest, highest)
+        }
+    }
+    
+    private static func partitionHoare(array: inout [Element], lowest: Int, highest: Int) -> Int {
+        let pivot = array[lowest]
+        var i = lowest - 1
+        var j = highest + 1
+        
+        while true {
+            i += 1
+            
+            while array[i] < pivot { i += 1 }
+            j -= 1
+            
+            while array[j] > pivot {j -= 1 }
+            if i >= j { return j }
+            
+            array.swapAt(i, j)
+        }
+    }
+    
+}
---
+++ extensions-kit/Extensions/Foundation/Array/Array+QuickSortLomutoScheme.swift
@@ -0,0 +1,39 @@
+//
+//  Array+QuickSortLomutoScheme.swift
+//  extensions-kit
+//
+//  Created by Astemir Eleev on 29/09/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+public extension Array where Element: Comparable {
+    
+    public static func quickSortLomuto(array: inout [Element], lowest: Int, highest: Int)  {
+        if lowest < highest {
+            let pivot = Array.partitionLomuto(array: &array, lowest: lowest, highest: highest)
+            
+            Array.quickSortLomuto(array: &array, lowest: lowest, highest: pivot - 1)
+            Array.quickSortLomuto(array: &array, lowest: pivot + 1, highest: highest)
+        } else {
+            debugPrint(#function + " lowest param is bigger than highest: ", lowest, highest)
+        }
+    }
+    
+    private static func partitionLomuto(array: inout [Element], lowest: Int, highest: Int) -> Int {
+        let pivot = array[highest]
+        var i = lowest
+        
+        for j in lowest..<highest {
+            if array[j] <= pivot {
+                array.swapAt(i, j)
+                i += 1
+            }
+        }
+        
+        array.swapAt(i, highest)
+        return i
+    }
+    
+}
