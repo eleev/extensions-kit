b9908762491afa2240d4cce7d5776b4da77f5545
Astemir Eleev <astemireleev@gmail.com>
2018-09-29 10:44:02 +0300

    Implemented Array+BubbleSort and Array+InsertionSort extensions

---
+++ extensions-kit/Extensions/Foundation/Array/Array+BubbleSort.swift
@@ -0,0 +1,29 @@
+//
+//  Array+BubbleSort.swift
+//  extensions-kit
+//
+//  Created by Astemir Eleev on 29/09/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+public extension Array where Element: Comparable {
+    
+    public mutating func bubbleSorted(order sign: (Element, Element) -> Bool) {
+        let count = self.count
+        
+        for _ in 0...count {
+            for value in 1...count - 2 {
+                if sign(self[value - 1], self[value]) {
+                    // An alternative to the manual `swap`
+                    // self.swapAt(value - 1, value)
+                    let largerValue = self[value - 1]
+                    self[value - 1] = self[value]
+                    self[value] = largerValue
+                }
+            }
+        }
+    }
+    
+}
---
+++ extensions-kit/Extensions/Foundation/Array/Array+InsertionSort.swift
@@ -0,0 +1,31 @@
+//
+//  Array+InsertionSort.swift
+//  extensions-kit
+//
+//  Created by Astemir Eleev on 29/09/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+public extension Array where Element: Comparable {
+    
+    // Sorts self with the given order (<, >) using the Insertion Sort algorithm
+    public mutating func insertionSort(order: (Element, Element) -> Bool) -> Array {
+        
+        if self.count <= 1 { return self }
+        
+        for i in 1..<self.count {
+            let temp = self[i]
+            var j = i
+            
+            while j > 0 && order(self[j - 1], temp) {
+                self[j] = self[j - 1]
+                j -= 1
+            }
+            self[j] = temp
+        }
+        return self
+    }
+    
+}
