d136126680f3921fc7af79bd55ca4a2eb02c6509
Astemir Eleev <astemireleev@gmail.com>
2018-09-29 10:45:11 +0300

    Implemented Array+ShellSort extension

---
+++ extensions-kit/Extensions/Foundation/Array/Array+ShellSort.swift
@@ -0,0 +1,32 @@
+//
+//  Array+ShellSort.swift
+//  extensions-kit
+//
+//  Created by Astemir Eleev on 29/09/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+public extension Array where Element: Comparable {
+    
+    // Shell sort is an improved version of insertion sort. The original list is broken into smaller sublists and then individually sorted using insertion sort.
+    public mutating func shellSorted(order sign: (Element, Element) -> Bool) {
+        var subcount = self.count / 2
+        
+        while subcount > 0 {
+            for i in 0..<self.count {
+                let temp = self[i]
+                var j = i
+                
+                while j >= subcount && sign(self[j - subcount], temp) {
+                    self[j] = self[j - subcount]
+                    j -= subcount
+                }
+                self[j] = temp
+            }
+            subcount /= 2
+        }
+        
+    }
+}
