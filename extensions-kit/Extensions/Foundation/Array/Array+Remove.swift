fc0e7ff86161df0da6402411d3d68c9b8d8660ec
Astemir Eleev <astemireleev@gmail.com>
2018-09-29 10:28:55 +0300

    Implemented Array+Remove extension

---
+++ extensions-kit/Extensions/Foundation/Array/Array+Remove.swift
@@ -0,0 +1,31 @@
+//
+//  Array+Remove.swift
+//  extensions-kit
+//
+//  Created by Astemir Eleev on 29/09/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+/// The extension adds support for convenience 'remove' methods for single elements, varargs and arrays of elements
+public extension Array where Element: Equatable {
+  
+    /// Removes an Element by mutating the array
+    @discardableResult
+    public mutating func remove(object: Element) -> Element? {
+        return index(of: object).map { self.remove(at: $0) }
+    }
+  
+    /// Removes a number of Elements by mutating the origianl array
+    public mutating func remove(objects: Element...) {
+        remove(objects: objects)
+    }
+    
+    /// Removes an array of Elements by mutating the origial array
+    public mutating func remove(objects: [Element]) {
+        objects.forEach {
+            self.remove(object: $0)
+        }
+    }
+}
