e108d624af2f6bf9bf3310955107708b43e4d859
Astemir Eleev <astemireleev@gmail.com>
2018-10-03 16:19:24 +0300

    Implemented UICollectionView extensions

---
+++ extensions-kit/Extensions/UIKit/UICollectionView/UICollectionView+Safety.swift
@@ -0,0 +1,20 @@
+//
+//  UICollectionView+Safety.swift
+//  extensions-kit
+//
+//  Created by Astemir Eleev on 03/10/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import UIKit
+
+public extension UICollectionView {
+    
+    /// Checks if the index path is valid
+    ///
+    /// - Parameter indexPath: is an IndexPath to check for
+    /// - Returns: a boolean value that indicates whether the specified IndexPath is valid or not
+    public func isValid(indexPath: IndexPath) -> Bool {
+        return indexPath.section < numberOfSections && indexPath.row < numberOfItems(inSection: indexPath.section)
+    }
+}
---
+++ extensions-kit/Extensions/UIKit/UICollectionView/UICollectionView+ScrollingUtils.swift
@@ -0,0 +1,42 @@
+//
+//  UICollectionView+ScrollingUtils.swift
+//  extensions-kit
+//
+//  Created by Astemir Eleev on 03/10/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import UIKit
+
+public extension UICollectionView {
+    
+    /// Scrolls to the bottom of the UICollectionView
+    ///
+    /// - Parameters:
+    ///     - animated: is a boolean parameter indicating whether the scrolling should be performed with animation
+    public func scrollToBottom(animated: Bool = true) {
+        let bottomOffset = CGPoint(x: 0, y: contentSize.height - bounds.size.height)
+        setContentOffset(bottomOffset, animated: animated)
+    }
+    
+    /// Scrolls to the top of the UICollectionView
+    ///
+    /// - Parameters:
+    ///     - animated: is a boolean parameter indicating wherher the scrolling should be performed with animation
+    public func scrollToTop(animated: Bool = true) {
+        setContentOffset(.zero, animated: animated)
+    }
+    
+    /// Wrapper that safely scrolls to the speficied index path
+    ///
+    /// - Parameters:
+    ///     - indexPath: is an IndexPath type that specifies the target scrolling position
+    ///     - scrollPosition: is the target scroll position of the specified index path
+    ///     - animated: is a boolean value indicating whether the scrolling should be animated or not
+    public func safeScrollingToCell(at indexPath: IndexPath, at scrollPosition: UICollectionView.ScrollPosition, animated: Bool = true) {
+        guard indexPath.section < numberOfSections, indexPath.row < numberOfItems(inSection: indexPath.section) else { return }
+        scrollToItem(at: indexPath, at: scrollPosition, animated: animated)
+    }
+}
+
+
