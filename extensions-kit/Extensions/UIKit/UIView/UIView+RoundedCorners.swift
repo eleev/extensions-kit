d34864a5e6ba84472164039aca1a69f7e5cbcbf4
Astemir Eleev <astemireleev@gmail.com>
2018-10-03 16:19:11 +0300

    Implemented UIView extensions

---
+++ extensions-kit/Extensions/UIKit/UIView/UIView+RoundedCorners.swift
@@ -0,0 +1,28 @@
+//
+//  UIView+RoundedCorners.swift
+//  extensions-kit
+//
+//  Created by Astemir Eleev on 03/10/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import UIKit
+
+public extension UIView {
+    
+    /// Rounds the corners of self by masking the CALayer with a CAShapeLayer using UIBezierPath
+    ///
+    /// - Parameters:
+    ///     - corners: is an array of UIRectCorner types e.g. [.topLeft, .topRight]
+    ///     - radius: is a CGFloat value that describes the radius of the corders
+    public func round(corners: UIRectCorner, radius: CGFloat) {
+        let maskPath = UIBezierPath(
+            roundedRect: bounds,
+            byRoundingCorners: corners,
+            cornerRadii: CGSize(width: radius, height: radius))
+        
+        let shape = CAShapeLayer()
+        shape.path = maskPath.cgPath
+        layer.mask = shape
+    }
+}
---
+++ extensions-kit/Extensions/UIKit/UIView/UIView+Screenshot.swift
@@ -0,0 +1,21 @@
+//
+//  UIView+Screenshot.swift
+//  extensions-kit
+//
+//  Created by Astemir Eleev on 03/10/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import UIKit
+
+public extension UIView {
+    
+    /// Takes a screenshot of self, and returns an optional UIImage instance
+    public var screenshot: UIImage? {
+        UIGraphicsBeginImageContextWithOptions(layer.frame.size, false, 0)
+        defer { UIGraphicsEndImageContext() }
+        guard let context = UIGraphicsGetCurrentContext() else { return nil }
+        layer.render(in: context)
+        return UIGraphicsGetImageFromCurrentImageContext()
+    }
+}
