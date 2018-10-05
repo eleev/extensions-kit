1e024f0c8acda20e114b57fd452ed4478602e047
Astemir Eleev <astemireleev@gmail.com>
2018-10-05 11:40:04 +0300

    Implemented CGSize+Operators extension

---
+++ extensions-kit/Extensions/CoreGraphics/CGSize/CGSize+Operators.swift
@@ -0,0 +1,44 @@
+//
+//  CGSize+Operators.swift
+//  extensions-kit
+//
+//  Created by Astemir Eleev on 05/10/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import CoreGraphics
+
+public extension CGSize {
+    
+    public static func +(left: CGSize, right: CGSize) -> CGSize {
+        return CGSize(width: left.width + right.width, height: right.height + right.height)
+    }
+    
+    public static func +=(left: inout CGSize, right: CGSize){
+        left = left + right
+    }
+    
+    public static func -(left: CGSize, right: CGSize) -> CGSize {
+        return CGSize(width: left.width - right.width, height: right.height - right.height)
+    }
+    
+    public static func -=(left: inout CGSize, right: CGSize){
+        left = left - right
+    }
+    
+    public static func *(left: CGSize, right: CGSize) -> CGSize {
+        return CGSize(width: left.width * right.width, height: right.height * right.height)
+    }
+    
+    public static func *=(left: inout CGSize, right: CGSize) {
+        left = left * right
+    }
+    
+    public static func /(left: CGSize, right: CGSize) -> CGSize {
+        return CGSize(width: left.width / right.width, height: right.height / right.height)
+    }
+    
+    public static func /=(left: inout CGSize, right: CGSize) {
+        left = left / right
+    }
+}
