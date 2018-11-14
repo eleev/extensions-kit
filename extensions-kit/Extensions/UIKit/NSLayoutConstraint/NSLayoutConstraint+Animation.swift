ef7c2d8a31fa40a2ff9cf0c8597aa326a72edfb6
Astemir Eleev <astemireleev@gmail.com>
2018-11-14 16:29:09 +0300

    Implemented NSLayoutConstraint+Animation extension

---
+++ extensions-kit/Extensions/UIKit/NSLayoutConstraint/NSLayoutConstraint+Animation.swift
@@ -0,0 +1,28 @@
+//
+//  NSLayoutConstraint+Animation.swift
+//  extensions-kit
+//
+//  Created by Astemir Eleev on 14/11/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import UIKit
+
+public extension NSLayoutConstraint {
+    
+    public func setConstant(for value: CGFloat, animated: Bool = false) {
+        constant = value
+        
+        guard let first = firstItem as? UIView,
+            let superview = first.superview else {
+                return
+        }
+        
+        if animated == false {
+            superview.layoutIfNeeded()
+            return
+        }
+        
+        UIView.animate(withDuration: 0.3) { superview.layoutIfNeeded() }
+    }
+}
