1c21c7699aa89174b8364139a3783476c0145837
Astemir Eleev <astemireleev@gmail.com>
2018-11-16 13:16:46 +0300

    Implemented additional methods for getting `safe layout areas` for .left & .right insets

--- extensions-kit/Extensions/UIKit/UIApplication/UIApplication+SafeAreas.swift
+++ extensions-kit/Extensions/UIKit/UIApplication/UIApplication+SafeAreas.swift
@@ -11,15 +11,35 @@ import UIKit
 public extension UIApplication {
     
     public class func safeAreaBottom() -> CGFloat {
-        let window = UIApplication.shared.keyWindow ?? UIApplication.shared.windows.first
+        let window = getFirstSharedAppsWindow()
         let bottomPadding = window?.safeAreaInsets.bottom ?? 0.0
         return bottomPadding
     }
     
     public class func safeAreaTop() -> CGFloat {
-        let window = UIApplication.shared.keyWindow ?? UIApplication.shared.windows.first
+        let window = getFirstSharedAppsWindow()
         let bottomPadding = window?.safeAreaInsets.top ?? 0.0
         return bottomPadding
+    }
+    
+    public class func safeAreaLeft() -> CGFloat {
+        let window = getFirstSharedAppsWindow()
+        let leftPadding = window?.safeAreaInsets.left ?? 0.0
+        return leftPadding
+    }
+    
+    public class func safeAreaRight() -> CGFloat {
+        let window = getFirstSharedAppsWindow()
+        let rightPadding = window?.safeAreaInsets.right ?? 0.0
+        return rightPadding
+    }
+    
+}
+
+private extension UIApplication {
+    
+    private class func getFirstSharedAppsWindow() -> UIWindow? {
+        return UIApplication.shared.keyWindow ?? UIApplication.shared.windows.first
     }
 }
 
