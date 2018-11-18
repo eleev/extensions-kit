fe5ff903a041eba488655b1af0a9c360f741168f
Astemir Eleev <astemireleev@gmail.com>
2018-11-18 09:55:05 +0300

    Implemented right-to-left composition function

--- extensions-kit/Extensions/Foundation/Functions/FunctionalComposition.swift
+++ extensions-kit/Extensions/Foundation/Functions/FunctionalComposition.swift
@@ -13,8 +13,15 @@ precedencegroup ComposePrecedence {
 }
 
 infix operator ->> : ComposePrecedence
+infix operator <<- : ComposePrecedence
 
-/// Composes two functions into one by using functional composition
+/// Composes two functions into one by using functional composition (left-to-right order e.g. (x) = rhs(lhs(x)))
 public func ->> <T, U, V>(lhs: @escaping (T) -> U, rhs: @escaping (U) -> V) -> ((T) -> V) {
     return { rhs(lhs($0)) }
 }
+
+/// Composes two function into one by using functional compoisition (right-to-left order e.g. (x) = lhs(rhs(x)))
+public func <<- <T, U, V>(lhs: @escaping (U) -> V, rhs: @escaping (T) -> U) -> ((T) -> V) {
+    return { lhs(rhs($0)) }
+}
+
