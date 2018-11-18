6b936806a2986faf6625ad2d4c2e9828c0004573
Astemir Eleev <astemireleev@gmail.com>
2018-11-18 09:54:23 +0300

    Created Collection+SafeSubscript extension

---
+++ extensions-kit/Extensions/Foundation/Collection/Collection+SafeSubscript.swift
@@ -0,0 +1,17 @@
+//
+//  Collection+SafeSubscript.swift
+//  extensions-kit
+//
+//  Created by Astemir Eleev on 18/11/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+public extension Collection {
+    
+    /// Safely checks whether the collection is able to retreive an element for the given Index, otherwise it will return nil
+    public subscript(safe index: Index) -> Element? {
+        return indices.contains(index) ? self[index] : nil
+    }
+}
