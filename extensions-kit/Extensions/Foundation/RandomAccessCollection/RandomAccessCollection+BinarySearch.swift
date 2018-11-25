9162bb34485a4d442017c92d1371bc891aa45e18
Astemir Eleev <astemireleev@gmail.com>
2018-11-25 17:26:54 +0300

    Implemented Binary Search algorithm in a form of RandomAccessCollection extension

---
+++ extensions-kit/Extensions/Foundation/RandomAccessCollection/RandomAccessCollection+BinarySearch.swift
@@ -0,0 +1,43 @@
+//
+//  RandomAccessCollection+BinarySearch.swift
+//  extensions-kit
+//
+//  Created by Astemir Eleev on 25/11/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+public extension RandomAccessCollection {
+    
+    /// Binary Search algorithm for a given value with the closure that specified the order of the elements
+    public func binarySearch(for value: Iterator.Element,
+                             isOrderIncreasing: (Iterator.Element, Iterator.Element) -> Bool = { _, _ in true })
+        -> Index? {
+            guard !isEmpty else { return nil }
+            var left = startIndex
+            var right = index(before: endIndex)
+            
+            while left <= right {
+                let dist = distance(from: left, to: right)
+                let mid = index(left, offsetBy: dist/2)
+                let candidate = self[mid]
+                
+                if isOrderIncreasing(candidate, value) { left = index(after: mid)
+                } else if isOrderIncreasing(value, candidate) { right = index(before: mid)
+                } else {
+                    // The elements are equal, do nothing
+                }
+            }
+            // Element has not been found
+            return nil
+    }
+    
+}
+
+public extension RandomAccessCollection where Iterator.Element: Comparable {
+    
+    public func binarySearch(for value: Iterator.Element) -> Index? {
+        return binarySearch(for: value, isOrderIncreasing: <)
+    }
+}
