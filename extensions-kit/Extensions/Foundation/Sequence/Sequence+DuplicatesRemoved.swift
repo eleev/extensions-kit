a0c25e3f179d0d440131a1d138a1b16a2929bac0
Astemir Eleev <astemireleev@gmail.com>
2018-12-12 13:49:53 +0300

    Implemented Sequence+DuplicatesRemoved extension

---
+++ extensions-kit/Extensions/Foundation/Sequence/Sequence+DuplicatesRemoved.swift
@@ -0,0 +1,18 @@
+//
+//  Sequence+DuplicatesRemoved.swift
+//  extensions-kit
+//
+//  Created by Astemir Eleev on 12/12/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+public extension Sequence where Element: Equatable {
+    
+    /// Removes the duplicate elements and returns the new Sequence without duplicates if any
+    /// The complexity is O(log(n))
+    public func removeDuplicates() -> [Element] {
+        return reduce([], { $0.contains($1) ? $0 : $0 + [$1] })
+    }
+}
