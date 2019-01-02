506878e021b2f51c419a10a2e644f62ce76bd80a
Astemir Eleev <astemireleev@gmail.com>
2019-01-02 15:49:24 +0300

    Created NSObject+ClassName extension

---
+++ extensions-kit/Extensions/Foundation/NSObject/NSObject+ClassName.swift
@@ -0,0 +1,17 @@
+//
+//  NSObject+ClassName.swift
+//  extensions-kit
+//
+//  Created by Astemir Eleev on 02/01/2019.
+//  Copyright © 2019 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+public extension NSObject {
+    
+    /// Contains name of a class
+    public class var nameOfClass: String {
+        return NSStringFromClass(self).components(separatedBy: ".").last!
+    }
+}
