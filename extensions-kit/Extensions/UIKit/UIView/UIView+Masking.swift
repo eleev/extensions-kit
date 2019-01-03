93f394dea47249fc9c0ae04f61c66484ce0aa2a7
Astemir Eleev <astemireleev@gmail.com>
2019-01-03 13:56:14 +0300

    Implemented UIView+Masking extension

---
+++ extensions-kit/Extensions/UIKit/UIView/UIView+Masking.swift
@@ -0,0 +1,23 @@
+//
+//  UIView+Masking.swift
+//  extensions-kit
+//
+//  Created by Astemir Eleev on 03/01/2019.
+//  Copyright © 2019 Astemir Eleev. All rights reserved.
+//
+
+import UIKit.UIView
+
+public extension UIView {
+    
+    /// Masks the view with the specified UIRectCorner array and corner radius
+    public func mask(corners: UIRectCorner = [], with cornerRadius: CGFloat = 6) {
+        let maskPath = UIBezierPath(roundedRect: bounds,
+                                    byRoundingCorners: corners,
+                                    cornerRadii: CGSize(width: cornerRadius, height: cornerRadius))
+        let maskLayer = CAShapeLayer()
+        maskLayer.frame = bounds
+        maskLayer.path = maskPath.cgPath
+        layer.mask = maskLayer
+    }
+}
