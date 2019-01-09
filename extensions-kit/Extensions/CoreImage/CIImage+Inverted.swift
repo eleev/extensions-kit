997dd35316b94705174697bc3f9a07297ac4dc2f
Astemir Eleev <astemireleev@gmail.com>
2019-01-09 15:36:40 +0300

    Implemented CIImage+Inverted extension

---
+++ extensions-kit/Extensions/CoreImage/CIImage+Inverted.swift
@@ -0,0 +1,20 @@
+//
+//  CIImage+Inverted.swift
+//  extensions-kit
+//
+//  Created by Astemir Eleev on 08/01/2019.
+//  Copyright © 2019 Astemir Eleev. All rights reserved.
+//
+
+import CoreImage.CIImage
+
+public extension CIImage {
+    
+    /// Inverts the colors of `self`
+    public var inverted: CIImage? {
+        guard let invertedColorFilter = CIFilter(name: "CIColorInvert") else { return nil }
+        
+        invertedColorFilter.setValue(self, forKey: "inputImage")
+        return invertedColorFilter.outputImage
+    }
+}
