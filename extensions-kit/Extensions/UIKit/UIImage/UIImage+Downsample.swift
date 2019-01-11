af6a88cf0af3ca27f4cdeed9584113146fc09bd6
Astemir Eleev <astemireleev@gmail.com>
2019-01-11 17:50:32 +0300

    Implemented UIImage+Downsample extension

---
+++ extensions-kit/Extensions/UIKit/UIImage/UIImage+Downsample.swift
@@ -0,0 +1,39 @@
+//
+//  UIImage+Downsample.swift
+//  extensions-kit
+//
+//  Created by Astemir Eleev on 11/01/2019.
+//  Copyright © 2019 Astemir Eleev. All rights reserved.
+//
+
+import UIKit
+
+public extension UIImage {
+    
+    /// Downsamples the imput image to the specified point size and scale factor. Can be used to present the thumbnails, supports caching. The best way to unilize this method is to use GCD in order to delegate the work off the main thread, then use the .main dispatch queue to sync the results.
+    ///
+    /// There is a possible caveat when using this method intensively by repeatedly creating dispatch queues. That may lead to Thread Explosion. If you need to process many images, for instances for `UITableView` or `UICollectionView`, pelase use a serial dispatch queue and asyncronously delegate the work. That will prevent your downsampling code to cause Thread Explosion and will keep the performance at a decent level.
+    ///
+    /// - Parameters:
+    ///   - url: a URL instnace that that specifies the location of an image
+    ///   - pointSize: is the target size represented by `CGSize` that you wish to get
+    ///   - scale: is the scale factor that is represented by `CGFloat`
+    /// - Returns: an optional `UIImage` instnace that holds the downsampled image, or `nil` if either `CGImageSourceCreateWithURL` or `CGImageSourceCreateThumbnailAtIndex` has failed a `guard` statement
+    public func downsample(imageAt url: URL, to pointSize: CGSize, scale: CGFloat) -> UIImage? {
+        let imageSourceOptions = [kCGImageSourceShouldCache: false] as CFDictionary
+        guard let imageSource = CGImageSourceCreateWithURL(url as CFURL, imageSourceOptions) else { return nil }
+        
+        let maxDimensionsInPixels = max(pointSize.width, pointSize.height) * scale
+        let downsampleOptions = [
+            kCGImageSourceCreateThumbnailFromImageAlways : true,
+            kCGImageSourceShouldCacheImmediately : true,
+            kCGImageSourceCreateThumbnailWithTransform : true,
+            kCGImageSourceThumbnailMaxPixelSize : maxDimensionsInPixels
+        ] as CFDictionary
+        
+        guard let downsampledImage = CGImageSourceCreateThumbnailAtIndex(imageSource, 0, downsampleOptions) else { return nil }
+        
+        return .init(cgImage: downsampledImage)
+    }
+    
+}
