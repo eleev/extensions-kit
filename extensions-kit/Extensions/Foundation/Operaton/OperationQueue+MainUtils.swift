be2c479c53ebe4ccb64f5114aea23aae9694533b
Astemir Eleev <astemireleev@gmail.com>
2019-01-12 15:33:39 +0300

    Implemented OperationQueue+MainUtils extension

---
+++ extensions-kit/Extensions/Foundation/Operaton/OperationQueue+MainUtils.swift
@@ -0,0 +1,22 @@
+//
+//  OperationQueue+MainUtils.swift
+//  extensions-kit
+//
+//  Created by Astemir Eleev on 11/01/2019.
+//  Copyright © 2019 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+public extension OperationQueue {
+    
+    /// Checks whether the current queue is the main
+    public static var isMain: Bool {
+        return main == current
+    }
+    
+    /// Executes an operation on the main operation queue
+    public static func onMain(_ operation: @escaping () -> Void) {
+        OperationQueue.main.addOperation(operation)
+    }
+}
