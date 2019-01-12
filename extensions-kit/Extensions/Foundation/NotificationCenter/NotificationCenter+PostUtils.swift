b25f715d43cd7d52da2f6cd1ea31fd4b533bb6ba
Astemir Eleev <astemireleev@gmail.com>
2019-01-12 15:32:43 +0300

    Implemented NotificationCenter+PostUtils extension

---
+++ extensions-kit/Extensions/Foundation/NotificationCenter/NotificationCenter+PostUtils.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationCenter+PostUtils.swift
+//  extensions-kit
+//
+//  Created by Astemir Eleev on 11/01/2019.
+//  Copyright © 2019 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+public extension NotificationCenter {
+    
+    /// Posts a notification by using the `default` notification center
+    public static func post(notification name: Foundation.Notification.Name, object: Any? = nil) {
+        NotificationCenter.default.post(name: name, object: object)
+    }
+}
