34ea12a5d2322b951caad16260355cb53055c5db
Astemir Eleev <astemireleev@gmail.com>
2019-01-24 16:31:06 +0300

    Implemented NSObjectProtocol+KVC+KVO extension

---
+++ extensions-kit/Extensions/Foundation/NSObjectProtocol/NSObjectProtocol+KVO+KVC.swift
@@ -0,0 +1,23 @@
+//
+//  NSObjectProtocol+KVO+KVC.swift
+//  extensions-kit
+//
+//  Created by Astemir Eleev on 24/01/2019.
+//  Copyright © 2019 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+public extension NSObjectProtocol where Self: NSObject {
+    
+    @discardableResult public func observe<Value>(for keyPath: KeyPath<Self, Value>, onChange: @escaping (Value) -> ()) -> NSKeyValueObservation {
+        return observe(keyPath, options: [.initial, .new], changeHandler: { (_, change) in
+            guard let newValue = change.newValue else { return }
+            onChange(newValue)
+        })
+    }
+    
+    @discardableResult public func bind<Value, Target>(keyPath: KeyPath<Self, Value>, to target: Target, at targetKeyPath: ReferenceWritableKeyPath<Target, Value>) -> NSKeyValueObservation {
+        return observe(for: keyPath, onChange: { target[keyPath: targetKeyPath] = $0 })
+    }
+}
